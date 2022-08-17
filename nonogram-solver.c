a0745fef36b96c93f2c5eb1add359d914df79735
pjhalpha <pjhalpha@gmail.com>
2022-08-17 23:30:22 +0900

    perf: change function guessing the line direction

--- nonogram-solver.c
+++ nonogram-solver.c
@@ -9,13 +9,12 @@
 struct TableList {
     int table[MAX_TABLE_SIZE + 1][MAX_TABLE_SIZE + 1];
     struct TableList *prev_table;
-} table_struct = {{0, }, NULL}, *table_struct_ptr = &table_struct;
+} table_struct = {{0, }, NULL}, *init_table_struct_ptr = &table_struct, *table_struct_ptr = &table_struct;
 int table_size[2];
 int clue[2][MAX_TABLE_SIZE][MAX_CLUE_SIZE + 1];
 int line[MAX_TABLE_SIZE], line_size;
 int line_clue[MAX_TABLE_SIZE], line_clue_size, line_clue_check[MAX_TABLE_SIZE];
 int new_line[MAX_TABLE_SIZE], new_line_case[MAX_TABLE_SIZE], new_line_check;
-int guess;
 
 int solve(int table_remain);
 int solveLine(int line_clue_sum, int line_clue_loc, int line_case_loc);
@@ -140,33 +139,31 @@ int solve(int table_remain) {
         int g_line_clue[MAX_TABLE_SIZE], g_line_clue_size;
         int g_new_line[MAX_TABLE_SIZE];
 
-        guess = 1;
-
-        g_line_size = table_size[1];
-        for (table_row = 0; table_row < table_size[0]; table_row++) {
-            if (table_struct_ptr->table[table_row][MAX_TABLE_SIZE] == 2) {
+        g_line_size = table_size[0];
+        for (table_col = 0; table_col < table_size[1]; table_col++) {
+            if (table_struct_ptr->table[MAX_TABLE_SIZE][table_col] == 2) {
                 continue;
             }
 
-            table_struct_ptr->table[table_row][MAX_TABLE_SIZE] = 2;
+            table_struct_ptr->table[MAX_TABLE_SIZE][table_col] = 2;
 
-            for (table_col = 0; table_col < g_line_size; table_col++) {
-                g_line[table_col] = table_struct_ptr->table[table_row][table_col];
-                g_new_line[table_col] = 0;
+            for (table_row = 0; table_row < g_line_size; table_row++) {
+                g_line[table_row] = table_struct_ptr->table[table_row][table_col];
+                g_new_line[table_row] = 0;
 
-                if (g_line[table_col] == 0) {
+                if (g_line[table_row] == 0) {
                     table_remain--;
                 }
             }
             
-            g_line_clue_size = clue[0][table_row][MAX_CLUE_SIZE];
+            g_line_clue_size = clue[1][table_col][MAX_CLUE_SIZE];
             line_clue_sum = 0;
             for (line_clue_loc = 0; line_clue_loc < g_line_clue_size; line_clue_loc++) {
-                g_line_clue[line_clue_loc] = clue[0][table_row][line_clue_loc];
+                g_line_clue[line_clue_loc] = clue[1][table_col][line_clue_loc];
                 line_clue_sum += g_line_clue[line_clue_loc];
             }
-            
-            return guessLine(g_line, g_line_size, g_line_clue, g_line_clue_size, line_clue_sum, 0, g_new_line, -1, table_row, table_remain);
+
+            return guessLine(g_line, g_line_size, g_line_clue, g_line_clue_size, line_clue_sum, 0, g_new_line, -1, table_col, table_remain);
         }
     }
 }
@@ -249,19 +246,19 @@ int solveLine(int line_clue_sum, int line_clue_loc, int line_case_loc) {
     }
 }
 
-int guessLine(int g_line[], int g_line_size, int g_line_clue[], int g_line_clue_size, int line_clue_sum, int line_clue_loc, int g_new_line[], int g_new_line_loc, int table_row, int table_remain) {
+int guessLine(int g_line[], int g_line_size, int g_line_clue[], int g_line_clue_size, int line_clue_sum, int line_clue_loc, int g_new_line[], int g_new_line_loc, int table_col, int table_remain) {
     if (g_new_line_loc == -1) {
         // return 0: This guessed table is the solution, 1: This guessed table is impossible
 
         if (g_line[0] != -1) {
-            if (guessLine(g_line, g_line_size, g_line_clue, g_line_clue_size, line_clue_sum, 0, g_new_line, 0, table_row, table_remain) == 0) {
+            if (guessLine(g_line, g_line_size, g_line_clue, g_line_clue_size, line_clue_sum, 0, g_new_line, 0, table_col, table_remain) == 0) {
                 return 0;
             }
         }
         for (g_new_line_loc = 0; (g_new_line_loc < g_line_size - g_line_clue_size - line_clue_sum + 1) && (g_line[g_new_line_loc] != 1); g_new_line_loc++) {
             g_new_line[g_new_line_loc] = -1;
             
-            if (guessLine(g_line, g_line_size, g_line_clue, g_line_clue_size, line_clue_sum, 0, g_new_line, g_new_line_loc + 1, table_row, table_remain) == 0) {
+            if (guessLine(g_line, g_line_size, g_line_clue, g_line_clue_size, line_clue_sum, 0, g_new_line, g_new_line_loc + 1, table_col, table_remain) == 0) {
                 return 0;
             }
         }
@@ -291,7 +288,7 @@ int guessLine(int g_line[], int g_line_size, int g_line_clue[], int g_line_clue_
 
                 g_new_line[g_new_line_loc] = -1;
 
-                if (guessLine(g_line, g_line_size, g_line_clue, g_line_clue_size, line_clue_sum, line_clue_loc, g_new_line, g_new_line_loc + 1, table_row, table_remain) == 0) {
+                if (guessLine(g_line, g_line_size, g_line_clue, g_line_clue_size, line_clue_sum, line_clue_loc, g_new_line, g_new_line_loc + 1, table_col, table_remain) == 0) {
                     return 0;
                 }
             }
@@ -310,23 +307,25 @@ int guessLine(int g_line[], int g_line_size, int g_line_clue[], int g_line_clue_
 
             struct TableList *g_table_struct_ptr = malloc(sizeof(struct TableList));
             g_table_struct_ptr->prev_table = table_struct_ptr;
-            for (g_table_row = 0; g_table_row < table_size[0]; g_table_row++) {
-                for (g_table_col = 0; g_table_col < table_size[1]; g_table_col++) {
+            for (g_table_col = 0; g_table_col < table_size[1]; g_table_col++) {
+                for (g_table_row = 0; g_table_row < table_size[0]; g_table_row++) {
                     g_table_struct_ptr->table[g_table_row][g_table_col] = table_struct_ptr->table[g_table_row][g_table_col];
                 }
             
-                g_table_struct_ptr->table[g_table_row][MAX_TABLE_SIZE] = table_struct_ptr->table[g_table_row][MAX_TABLE_SIZE];
+                g_table_struct_ptr->table[MAX_TABLE_SIZE][g_table_col] = table_struct_ptr->table[MAX_TABLE_SIZE][g_table_col];
             }
-            for (g_table_col = 0; g_table_col < table_size[1]; g_table_col++) {
-                if (table_struct_ptr->table[MAX_TABLE_SIZE][g_table_col] == 2) {
-                    g_table_struct_ptr->table[MAX_TABLE_SIZE][g_table_col] = 2;
-                } else {
-                    g_table_struct_ptr->table[MAX_TABLE_SIZE][g_table_col] = 0;
+            for (g_table_row = 0; g_table_row < table_size[0]; g_table_row++) {
+                if (table_struct_ptr->table[g_table_row][MAX_TABLE_SIZE] == 2) {
+                    g_table_struct_ptr->table[g_table_row][MAX_TABLE_SIZE] = 2;
+
+                    continue;
                 }
+                
+                g_table_struct_ptr->table[g_table_row][MAX_TABLE_SIZE] = 0;
             }
             
             for (g_new_line_loc = 0; g_new_line_loc < g_line_size; g_new_line_loc++) {
-                g_table_struct_ptr->table[table_row][g_new_line_loc] = g_new_line[g_new_line_loc];
+                g_table_struct_ptr->table[g_new_line_loc][table_col] = g_new_line[g_new_line_loc];
             }
 
             table_struct_ptr = g_table_struct_ptr;
@@ -346,23 +345,23 @@ void printTable(void) {
 
     for (table_row = 0; table_row < table_size[0]; table_row++) {
         for (table_col = 0; table_col < table_size[1]; table_col++) {
-            switch (table_struct_ptr->table[table_row][table_col]) {
+            switch (init_table_struct_ptr->table[table_row][table_col]) {
                 case -1:
                     printf("%2c", '.');
 
                     break;
                 case 0:
-                    printf("%2c", '-');
-
-                    break;
-                case 1:
-                    if (guess == 0) {
-                        printf("%2c", '#');
+                    if (table_struct_ptr->table[table_row][table_col] == -1) {
+                        printf("%2c", ',');
 
                         break;
                     }
 
                     printf("%2c", '$');
+
+                    break;
+                case 1:
+                    printf("%2c", '#');
 
                     break;
             }
